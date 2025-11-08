b340955da0ddc9fd3266a77cf09fc044aa523aa4
Michael Hodges <Michael.Hodges1985@yahoo.com>
2025-11-08 16:55:17 -0600

    now we added the Models

---
+++ my-sparkle-api/Models/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace MySparkleHeart.Api.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+
+        public string PhoneNumber { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
